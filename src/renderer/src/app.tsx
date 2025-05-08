37db80529c066aac2562db492b7e1f909393f154
haku <hakushinring@gmail.com>
2025-05-09 04:57:28 +0800

    chore: update

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,13 +1,6 @@
-
-
-// 创建一个路由配置示例
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './home';
-import About from './about';
-
-// 示例组件 - 在实际应用中你会导入真正的组件
-// const Home = () => <div>首页</div>;
-// const About = () => <div>关于页面</div>;
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from './home'
+import About from './about'
 
 const App: React.FC = () => {
   return (
@@ -17,7 +10,7 @@ const App: React.FC = () => {
         <Route path="/about" element={<About />} />
       </Routes>
     </HashRouter>
-  );
+  )
 }
 
-export default App;
+export default App
